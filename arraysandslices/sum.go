ae3caa2f4af6935eaf97d967e00b442ac8cd7ad4
Rahul Pandita <rpandita1999@outlook.com>
2025-10-07 05:13:09 +0000

    test and function to sum all slices of ints

--- arraysandslices/sum.go
+++ arraysandslices/sum.go
@@ -1,9 +1,19 @@
 package arraysandslices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, num := range numbers {
 		sum += num
 	}
 	return sum
+}
+
+// numbersToSum will be a slice of type [][]int
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
--- arraysandslices/sum_test.go
+++ arraysandslices/sum_test.go
@@ -1,14 +1,28 @@
 package arraysandslices
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 6
 
-	if got != want {
-		t.Errorf("got %d, want %d, given %v", got, want, numbers)
+		if got != want {
+			t.Errorf("got %d, want %d, given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
