89cde098e0092442a2263e7340b7f1de7e60843c
Rahul Pandita <rpandita1999@outlook.com>
2025-10-09 02:32:49 +0000

    create concurrent function to measure latency of HTTP Get request, send result through chan

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+type result struct {
+	url     string
+	err     error
+	latency time.Duration
+}
+
+func get(url string, ch chan<- result) {
+	start := time.Now()
+
+	if resp, err := http.Get(url); err != nil {
+		ch <- result{url, err, 0}
+	} else {
+		t := time.Since(start).Round(time.Millisecond)
+		ch <- result{url, nil, t}
+		resp.Body.Close()
+	}
+}
