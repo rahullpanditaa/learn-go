13f5695db13ba1b5acc0156388e64cf5cddd0c01
Rahul Pandita <rpandita1999@outlook.com>
2025-10-10 06:07:04 +0000

    use json.Marshal to encode struct

--- main.go
+++ main.go
@@ -1,33 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
 )
 
+type Response struct {
+	Page  int      `json:"page"`
+	Words []string `json:"words,omitempty"`
+}
+
 func main() {
-	chans := []chan int{
-		make(chan int),
-		make(chan int),
-	}
+	r := &Response{Page: 1, Words: []string{"up", "in", "out"}}
 
-	for i := range chans {
-		go func(i int, ch chan<- int) {
-			for {
-				time.Sleep(time.Duration(i) * time.Second)
-				ch <- i
-			}
-		}(i+1, chans[i])
-	}
-
-	for range 12 {
-		// i1 := <-chans[0]
-		// i2 := <-chans[1]
-		select {
-		case m0 := <-chans[0]:
-			fmt.Println("received", m0)
-		case m1 := <-chans[1]:
-			fmt.Println("received", m1)
-		}
-	}
+	// Marshal returns a byte slice and an error
+	j, _ := json.Marshal(r)
+	fmt.Println(string(j))
+	fmt.Printf("%#v\n", r)
 }
