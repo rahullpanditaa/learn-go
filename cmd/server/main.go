9d4fc71171814143674a31b77b4c1bef03c72e2c
Rahul Pandita <rpandita1999@outlook.com>
2025-10-11 02:41:59 +0000

    code sends get request to a url, acts as a server to local request

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,53 @@
 package server
 
 import (
-	"fmt"
+	"encoding/json"
+	"html/template"
+	"io"
 	"net/http"
+	"os"
 )
 
-const url = "https://jsonplaceholder.typicode.com"
-
 type todo struct {
+	UserID    int    `json:"userID"`
 	ID        int    `json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
 
+var form = `
+<h1>Todo #{{.ID}}</h1>
+<div>{{printf "User %d" .UserID}}</div>
+<div>{{printf "%s (completed: %t)" .Title .Completed}}</div>`
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world, from %s\n", r.URL.Path[1:])
+	const base = "https://jsonplaceholder.typicode.com"
+
+	resp, err := http.Get(base + os.Args[1])
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var item todo
+	err = json.Unmarshal(body, &item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl := template.New("mine")
+	tmpl.Parse(form)
+	tmpl.Execute(w, item)
+
 }
 
 func main() {
